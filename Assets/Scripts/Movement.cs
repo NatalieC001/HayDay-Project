4e6f376ea76fa8c734cc91e64cb5c635a0b4c970
Brokenthought <2finnwilliams@gmail.com>
2015-06-13 20:40:08 +0100

    Camera and movement changes

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -13,18 +13,38 @@ public class Movement : MonoBehaviour {
 	float moveSpeed;
 	float verticalVel;  // Used for continuing momentum while in air    
 	CharacterController controller;
-	
+
+    Camera mainCamera;
+
+    GameObject player;
+
+    Animator animator;
+
+    Vector3 difVec;
 	void Start()
 	{
 		controller = (CharacterController)GetComponent(typeof(CharacterController));
+
+        mainCamera = (Camera)GameObject.Find("MainCamera").GetComponent<Camera>();
+        animator = GetComponent<Animator>();
+        player = GameObject.Find("Player");
+        difVec = new Vector3(-10.3f, 16.1f, -11.6f);
+        print(difVec);
 	}
 	float UpdateMovement()
 	{
+
+        
 		// Movement
 		float x = Input.GetAxis("Horizontal");
 		float z = Input.GetAxis("Vertical");
 		
 		Vector3 inputVec = new Vector3(x, 0, z);
+
+
+
+        Vector3 newCamVec = (mainCamera.transform.position - player.transform.position) - difVec;
+
 		inputVec *= Speed;
 		
 		controller.Move((inputVec + Vector3.up * -gravity + new Vector3(0, verticalVel, 0)) * Time.deltaTime);
@@ -34,7 +54,9 @@ public class Movement : MonoBehaviour {
 			transform.rotation = Quaternion.Slerp(transform.rotation, 
 			                                      Quaternion.LookRotation(inputVec), 
 			                                      Time.deltaTime * rotationDamping);
-		
+
+
+        mainCamera.transform.position = player.transform.position + difVec;
 		return inputVec.magnitude;
 	}
 	void Update()
@@ -59,7 +81,13 @@ public class Movement : MonoBehaviour {
 		// Actually move the character
 		moveSpeed = UpdateMovement();
 
-		Debug.Log ("Move Speed: " + moveSpeed.ToString());
+      //  if (Input.GetKey("return"))
+        {
+            animator.SetFloat("Speed", moveSpeed, 0.01f, .01f);
+        }
+        
+
+		//Debug.Log ("Move Speed: " + moveSpeed.ToString());
 		
 		if ( controller.isGrounded )
 			verticalVel = 0f;// Remove any persistent velocity after landing
