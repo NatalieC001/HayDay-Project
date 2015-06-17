fbc5d81b6eff7cf693a82ba2dbe2df987c09f3ce
Brokenthought <2finnwilliams@gmail.com>
2015-06-17 21:15:12 +0100

    Added overly friendly cow
    Very basic AI and animation,
    I think its the right way to go about it though

---
+++ Assets/Scripts/CowMovement.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class  CowMovement : MonoBehaviour {
+
+    Transform player;
+    Animation _animation;
+
+    Animation anim;
+
+    public Rigidbody rb;
+    void Start()
+    {
+        
+        rb = GetComponent<Rigidbody>();
+
+        player = GameObject.Find("Player").transform;
+    
+        anim = GetComponent<Animation>();
+        anim.Play("walk");
+    }
+	
+
+
+	// Update is called once per frame
+	void Update () {
+
+      
+        Vector3 position = new Vector3(player.position.x, transform.position.y, player.position.z);
+        transform.LookAt(position);
+        transform.position += transform.forward*4*Time.deltaTime;
+
+	}
+
+
+
+}
