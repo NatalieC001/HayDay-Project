0c2eed1cd5ae028bc64b26ba060405a7f2547d25
John Walsh <john.walsh16609@gmail.com>
2016-03-29 22:22:56 +0100

    Added Menus for Google Play Services
    Added the Achievements & Leaderboard UI handlers
    Added to the new menu accessible from the main menu
    Other small bug fixes too complete
    Android build available - Submitted to store

---
+++ Assets/Scripts/NGUI/UIMiscScene.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using GooglePlayGames;
+using UnityEngine.SocialPlatforms;
+
+namespace IrishFarmSim
+{
+	public class UIMiscScene : MonoBehaviour 
+	{
+		public bool loadAchievements;
+
+		public void OnClick()
+		{
+			if(loadAchievements)
+			{
+				OpenA();
+			}
+			else
+			{
+				OpenL();
+			}
+		}
+
+		public void OpenA()
+		{
+			Social.ShowAchievementsUI();
+		}
+
+		public void OpenL()
+		{
+			Social.ShowLeaderboardUI();
+		}
+	}
+}
